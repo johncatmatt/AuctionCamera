a75fd2243c45a86cbbdfa9ba07ca4c5d8b97b278
Matthew Sansoucie <sansouciem@merrimack.edu>
2019-03-02 17:00:38 -0500

    Initial Commit

---
+++ AuctionCamera/PreviewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  VCPreview.swift
+//  AuctionCamera
+//
+//  Created by Matthew Sansoucie on 3/2/19.
+//  Copyright © 2019 Matthew Sansoucie. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    
+    @IBOutlet weak var photo: UIImageView!
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photo.image = self.image
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancelButton_TouchUpInside(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func saveButton_TouchUpInside(_ sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AuctionCamera/ViewController.swift
+++ AuctionCamera/ViewController.swift
@@ -7,14 +7,102 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCapturSession()
     }
 
+    func setupCaptureSession(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+    
+    
+    func setupDevice(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+         currentCamera = backCamera
+    }
+    
+    func setupInputOutput(){
+        do {
+        let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
+    
+        } catch{
+            print(error)
+        }
+    }
+    
+    
+    func setupPreviewLayer(){
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+    }
+    
+    func startRunningCapturSession(){
+        captureSession.startRunning()
+    }
+    
+    
+    @IBAction func cameraButton_TouchUpInside(_ sender: Any) {
+           let settings = AVCapturePhotoSettings()
+            photoOutput?.capturePhoto(with: settings, delegate: self)
+       // performSegue(withIdentifier: "showPhoto_Segue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showPhoto_Segue" {
+            let previewVC = segue.destination as! PreviewViewController
+            previewVC.image = self.image
+            
+        }
+    }
 
+}
+
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(){
+           // print(imageData)
+            image = UIImage(data: imageData)
+            performSegue(withIdentifier: "showPhoto_Segue", sender: nil)
+            
+        }
+    }
 }
 
